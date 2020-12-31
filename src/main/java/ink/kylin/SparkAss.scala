bf62b12487a560d250d6f26148422dada479fcb3
kylin <linuxmorebetter@gmail.com>
2020-12-31 23:11:22 +0800

    init

--- src/main/java/ink/kylin/SparkAss.scala
+++ src/main/java/ink/kylin/SparkAss.scala
@@ -1,7 +1,10 @@
 package ink.kylin
 
-import org.apache.spark.sql.SparkSession
+import java.util.Properties
+
+import org.apache.spark.sql.{SaveMode, SparkSession}
 import org.apache.spark.sql.functions._
+
 import scala.io.StdIn
 
 object SparkAss {
@@ -13,6 +16,21 @@ object SparkAss {
     val sc = spark.sparkContext
 
 
+
+    val mysql_user="root"
+    val mysql_pwd ="root"
+    val mysql_driver="com.mysql.jdbc.Driver"
+    val mysql_url = "jdbc:mysql://127.0.0.1:3306/airports?useUnicode=true&characterEncoding=UTF-8&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC"
+
+
+    val connectionProperties = new Properties
+    //connectionProperties.setProperty("dbtable", "data")
+    connectionProperties.setProperty("user", mysql_user)
+    connectionProperties.setProperty("password", mysql_pwd)
+    connectionProperties.setProperty("driver", mysql_driver)
+    connectionProperties.setProperty("truncate","true")
+
+
     val airports_df = spark.read.format("csv").option("header","true").csv("data/airports.csv")
     airports_df.show()
 
@@ -22,7 +40,7 @@ object SparkAss {
       "regexp_replace(`\"id,\"\"ident\"\"`,'\"','') as id_and_ident," +
       "regexp_replace(`\"\"type\"\"`,'\"','') as type," +
       "regexp_replace(`\"\"name\"\"`,'\"','') as name," +
-      "regexp_replace(`\"\"continent\"\"`,'\"','') as latitude_deg," +
+      "regexp_replace(`\"\"continent\"\"`,'\"','') as continent," +
       "regexp_replace(`\"\"iso_region\"\"`,'\"','') as iso_region," +
       "regexp_replace(`\"\"municipality\"\"`,'\"','') as municipality," +
       "regexp_replace(`\"\"scheduled_service\"\"`,'\"','') as scheduled_service," +
@@ -39,13 +57,15 @@ object SparkAss {
       .withColumn("ident",$"splitCols".getItem(1)).drop("id_and_ident","splitCols","`\"\"keywords\"\"\";`").cache()
 
     airports_clean.createOrReplaceTempView("airports")
-
+    airports_clean.printSchema()
+    airports_clean.write.mode(SaveMode.Overwrite).jdbc(mysql_url, "airports", connectionProperties)
     val countries_df = spark.read.format("csv").option("header","true").csv("data/countries.csv")
 
     val runways_df = spark.read.format("csv").option("header","true").csv("data/runways.csv")
     val airports_df_sql = spark.sql("select iso_country ,1 as count from airports ")
     val airports_top10 =  airports_df_sql.groupBy("iso_country").sum("count").orderBy(desc("sum(count)")).cache()
     //top10
+    airports_top10.withColumnRenamed("sum(count)","sum_count").write.mode(SaveMode.Overwrite).jdbc(mysql_url, "airports_top", connectionProperties)
 
     //low10
     val airports_low10 = airports_df_sql.groupBy("iso_country").sum("count").orderBy("sum(count)").cache()
@@ -55,11 +75,16 @@ object SparkAss {
     val  runways_df_sql = spark.sql("select le_ident ,1 as count from runways")
     val runways_top10 = runways_df_sql.groupBy("le_ident").sum("count").orderBy(desc("sum(count)")).cache()
 
+    runways_top10.withColumnRenamed("sum(count)","sum_count").write.mode(SaveMode.Overwrite).jdbc(mysql_url, "runways_top", connectionProperties)
+
     val runways_inner_join =  spark.sql("select a.iso_country,r.* from runways r inner join airports a on r.airport_ident=a.ident")
 
     runways_inner_join.createOrReplaceTempView("runways_inner")
+    runways_inner_join.write.mode(SaveMode.Overwrite).jdbc(mysql_url, "runways_inner", connectionProperties)
 
     val country_surface =spark.sql("select distinct concat_ws('-',iso_country,surface) as country_surface from runways_inner order by country_surface ").cache()
+
+    country_surface.write.mode(SaveMode.Overwrite).jdbc(mysql_url, "country_surface", connectionProperties)
 
     while (true){
       println("input 0:query ,1:report")
